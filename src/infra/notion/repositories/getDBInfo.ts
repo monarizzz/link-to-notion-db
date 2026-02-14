a05d12bfc85a290e327cf111cb560c34a9bbcb18
Cloud <monaniza@gmail.com>
2026-02-14 22:47:41 +0900

    ref Database -> DB

--- src/infra/notion/repositories/getDBInfo.ts
+++ src/infra/notion/repositories/getDBInfo.ts
@@ -1,10 +1,10 @@
 import { notion } from "@/libs/notion/utils/client";
 
 /* DATA_SOURCE_ID を取得するために使用 基本的には使わない */
-const getDatabaseInfo = async () => {
+const getDBInfo = async () => {
   return await notion.databases.retrieve({
     database_id: process.env.DATABASE_ID!,
   });
 };
 
-export default getDatabaseInfo;
+export default getDBInfo;
