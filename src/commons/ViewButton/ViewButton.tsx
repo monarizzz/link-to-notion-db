b5a04f7af7a17308ee2bae372636e46f75dc38c3
Cloud <monaniza@gmail.com>
2026-02-14 17:03:42 +0900

    feat view切り替えのボタン実装

---
+++ src/commons/ViewButton/ViewButton.tsx
@@ -0,0 +1,32 @@
+"use client";
+import Link from "next/link";
+import styles from "./ViewButton.module.css";
+import { usePathname } from "next/navigation";
+
+type Task = {
+  id: string;
+  name: string;
+};
+
+type Props = {
+  tasks: Task[];
+};
+
+const ViewButton = ({ tasks }: Props) => {
+  const pathname = usePathname();
+  return (
+    <div className={styles.root}>
+      {tasks.map((task) => (
+        <Link
+          key={task.id}
+          href={`${pathname}?v=${task.id}`}
+          className={styles.name}
+        >
+          {task.name}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default ViewButton;
