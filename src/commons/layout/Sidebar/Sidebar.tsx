2cdc1a7c449452fe5597685426a952812d61437a
Cloud <monaniza@gmail.com>
2026-02-14 21:18:46 +0900

    feat sidebar作成

---
+++ src/commons/layout/Sidebar/Sidebar.tsx
@@ -0,0 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./Sidebar.module.css";
+
+type Props = {
+  contents: content[];
+};
+
+type content = {
+  text: string;
+  icon: string;
+  path: string;
+};
+
+const Sidebar = ({ contents }: Props) => {
+  return (
+    <div className={styles.root}>
+      <div className={styles.contentGroup}>
+        <Link href={"/"} className={styles.home}>
+          <Image
+            src={"/sidebar/home.svg"}
+            alt={"home"}
+            width={30}
+            height={30}
+            className={styles.icon}
+          />
+        </Link>
+      </div>
+      <div className={styles.contentGroup}>
+        {contents.map((content) => (
+          <Link
+            href={`${content.path}`}
+            key={content.text}
+            className={styles.content}
+          >
+            <Image
+              src={`/sidebar/${content.icon}.svg`}
+              alt={`${content.text}`}
+              width={20}
+              height={20}
+              className={styles.icon}
+            />
+            {content.text}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
