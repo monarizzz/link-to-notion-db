c6cd187985c3184010b16fbb4454f96245f32a58
Cloud <monaniza@gmail.com>
2026-02-14 23:20:59 +0900

    fix 取得順を作成（降順）に

--- src/infra/notion/repositories/getDBRecords.ts
+++ src/infra/notion/repositories/getDBRecords.ts
@@ -4,6 +4,12 @@ import { notion } from "@/libs/notion/utils/client";
 const getDBRecords = async () => {
   const response = await notion.dataSources.query({
     data_source_id: process.env.DATA_SOURCE_ID!,
+    sorts: [
+      {
+        timestamp: "created_time",
+        direction: "descending",
+      },
+    ],
   });
   return response;
 };
