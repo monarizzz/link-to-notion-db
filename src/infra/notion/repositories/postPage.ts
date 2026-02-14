208950c2f8a57f156701d294c6072c8dd13abaf6
Cloud <monaniza@gmail.com>
2026-02-15 03:23:43 +0900

    feat postPage作成

---
+++ src/infra/notion/repositories/postPage.ts
@@ -0,0 +1,12 @@
+import { notion } from "@/libs/notion/utils/client";
+import type { CreatePageParameters } from "@notionhq/client/build/src/api-endpoints";
+
+const postPage = async (properties: CreatePageParameters["properties"]) => {
+  const response = await notion.pages.create({
+    parent: { database_id: process.env.DATABASE_ID! },
+    properties,
+  });
+  return response;
+};
+
+export default postPage;
