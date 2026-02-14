2fc2e6125edc22e1ad46d20bea533488fe697400
Cloud <monaniza@gmail.com>
2026-02-14 21:37:17 +0900

    fix Record -> ToRecord

---
+++ src/features/notion/types/toRecord.ts
@@ -0,0 +1,7 @@
+export type ToRecord = {
+  id: string;
+  taskName: string;
+  start: string;
+  end: string;
+  url: string;
+};
--- src/features/notion/utils/toRecord.ts
+++ src/features/notion/utils/toRecord.ts
@@ -1,8 +1,8 @@
 const toRecord = (records) => ({
   id: records.id,
-  taskName: records.properties.taskName.title[0]?.plain_text ?? "",
-  start: records.properties.workTime.date?.start ?? "",
-  end: records.properties.workTime.date?.end ?? "",
+  taskName: records.properties.taskName.title[0]?.plain_text,
+  start: records.properties.workTime.date?.start,
+  end: records.properties.workTime.date?.end,
   url: records.url,
 });
 
