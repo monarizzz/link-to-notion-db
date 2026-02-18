bb7807ddaff65e65a5cd72cfd49f6a9be523ab99
Cloud <monaniza@gmail.com>
2026-02-18 17:13:20 +0900

    fix import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import toRecord from "@/features/notion/utils/toRecord";
 import getDBRecords from "@/infra/notion/repositories/getDBRecords";
-import WorkLogDateGroup from "@/features/home/WorkLogDateGroup/components/WorkLogDateGroup";
+import HomePageMain from "@/features/home/homePageMain/components/HomePageMain";
 
 const Home = async () => {
   const allRecords = await getDBRecords();
   const data = allRecords.results.map((record) => toRecord(record));
-
   return (
-    <div>
-      <WorkLogDateGroup records={data} />
-    </div>
+    <>
+      <HomePageMain records={data} />
+    </>
   );
 };
 
