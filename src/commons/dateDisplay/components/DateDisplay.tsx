ac65a4e5ab2d317b67ea3fd7399ad1319e316f23
Cloud <monaniza@gmail.com>
2026-03-05 21:11:52 +0900

    style 変更

--- src/commons/dateDisplay/components/DateDisplay.tsx
+++ src/commons/dateDisplay/components/DateDisplay.tsx
@@ -5,8 +5,8 @@ type Props = {
 const DateDisplay = ({ date }: Props) => {
   //TODO: 今日の場合のラベルを追加する
   return (
-    <div>
-      <div className="text-xs mb-2">日付</div>
+    <div className="w-full">
+      <div className="text-xs mb-2.5">日付</div>
       <div className="font-dm-sans text-sm">
         {date?.toLocaleDateString("ja-JP")}
       </div>
