671da2de902c5cb4433eb1b6a073f2f48cc2edd3
Cloud <monaniza@gmail.com>
2026-03-07 01:14:41 +0900

    ref typo

--- src/features/addData/components/TimeField/TimeField.tsx
+++ src/features/addData/components/TimeField/TimeField.tsx
@@ -9,7 +9,7 @@ const TimeField = ({ text }: Props) => {
   return (
     <div className="flex gap-2.5 px-3.5 items-center">
       <Image src="/time.svg" width={10} height={10} alt="time" />
-      <span className="text-2xs font-medium  text-subtle-foreground pr-3.5">
+      <span className="text-2xs font-medium text-subtle-foreground pr-3.5">
         {text}
       </span>
       <div className="self-stretch h-auto py-4 ">
