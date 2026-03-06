9a7efe8d17042006e71a7dba1949cf17c1dea2b8
Cloud <monaniza@gmail.com>
2026-03-06 16:07:56 +0900

    fix "日付"を親で管理

--- src/features/addData/components/DateDisplay/DateDisplay.tsx
+++ src/features/addData/components/DateDisplay/DateDisplay.tsx
@@ -1,12 +1,11 @@
 type Props = {
-  date: Date | undefined;
+  date: Date;
 };
 
 const DateDisplay = ({ date }: Props) => {
   //TODO: 今日の場合のラベルを追加する
   return (
     <div className="w-full">
-      <div className="text-xs mb-2.5">日付</div>
       <div className="font-dm-sans text-sm">
         {date?.toLocaleDateString("ja-JP")}
       </div>
