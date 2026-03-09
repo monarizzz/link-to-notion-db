bbe6f35b0fed76e0059b8cff914727a8b47e771a
Cloud <monaniza@gmail.com>
2026-03-09 17:24:26 +0900

    fix 日付を含まない値が入力された場合の処理とテストを追加

--- src/commons/addData/entities/diffTime.ts
+++ src/commons/addData/entities/diffTime.ts
@@ -2,6 +2,6 @@ export const diffTime = (start: string, end: string): string => {
   const diff = Math.floor(
     (new Date(end).getTime() - new Date(start).getTime()) / 60000,
   );
-  if (diff <= 0) return "-";
+  if (isNaN(diff) || diff <= 0) return "-";
   return `${Math.floor(diff / 60)}時間 ${diff % 60}分`;
 };
