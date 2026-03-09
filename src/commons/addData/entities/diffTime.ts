a6125985c806dba26ab5a36247955918a0575ae1
Cloud <monaniza@gmail.com>
2026-03-09 14:41:21 +0900

    fix 日付を含む計算方法に変更

--- src/commons/addData/entities/diffTime.ts
+++ src/commons/addData/entities/diffTime.ts
@@ -1,7 +1,7 @@
 export const diffTime = (start: string, end: string): string => {
-  const [sh, sm] = start.split(":").map(Number);
-  const [eh, em] = end.split(":").map(Number);
-  const diff = eh * 60 + em - (sh * 60 + sm);
+  const diff = Math.floor(
+    (new Date(end).getTime() - new Date(start).getTime()) / 60000,
+  );
   if (diff <= 0) return "-";
   return `${Math.floor(diff / 60)}時間 ${diff % 60}分`;
 };
