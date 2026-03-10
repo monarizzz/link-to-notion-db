71a31eaa13f6597d87ffd76de879cba5094fb63e
Cloud <monaniza@gmail.com>
2026-03-11 01:29:40 +0900

    fix editBtn

--- src/features/addData/components/EditBtn/EditBtn.tsx
+++ src/features/addData/components/EditBtn/EditBtn.tsx
@@ -6,14 +6,15 @@ type Props = {
 
 const EditBtn = ({ onClick }: Props) => {
   return (
-    <Image
-      src="/pen.svg"
-      alt="pen"
-      width={10}
-      height={10}
-      className="pb-0.5 cursor-pointer"
-      onClick={onClick}
-    />
+    <button type="button" onClick={onClick} className="cursor-pointer">
+      <Image
+        src="/pen.svg"
+        alt="pen"
+        width={10}
+        height={10}
+        className="pb-0.5 cursor-pointer"
+      />
+    </button>
   );
 };
 
