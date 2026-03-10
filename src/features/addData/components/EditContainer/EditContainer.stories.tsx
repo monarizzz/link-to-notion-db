88c3484e2b71ffc3e677fabf24d971156e9992e7
Cloud <monaniza@gmail.com>
2026-03-11 01:34:51 +0900

    fix stories args

--- src/features/addData/components/EditContainer/EditContainer.stories.tsx
+++ src/features/addData/components/EditContainer/EditContainer.stories.tsx
@@ -12,4 +12,9 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {};
+export const Default: Story = {
+  args: {
+    hours: 2,
+    minutes: 0,
+  },
+};
