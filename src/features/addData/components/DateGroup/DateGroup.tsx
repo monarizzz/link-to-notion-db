aeb04178ee4f19665aa55f8824690797a325c108
Cloud <monaniza@gmail.com>
2026-03-12 01:19:23 +0900

    ref コミット忘れ

--- src/features/addData/components/DateGroup/DateGroup.tsx
+++ src/features/addData/components/DateGroup/DateGroup.tsx
@@ -1,29 +1,32 @@
 import CalendarPicker from "@/features/addData/components/CalendarPicker/CalendarPicker";
 import DateDisplay from "@/features/addData/components/DateDisplay/DateDisplay";
 import { Separator } from "@/libs/shadcn/assets/ui/separator";
-import dayjs from "dayjs";
 import DateLabel from "../DateLabel/DateLabel";
+import type { AddDataForm } from "@/commons/addData/components/AddData";
+import { Controller, useFormContext } from "react-hook-form";
 
-type Props = {
-  date: string;
-  setDate: (date: Date | undefined) => void;
-};
-
-const DateGroup = ({ date, setDate }: Props) => {
-  const dateObj = dayjs(date).toDate();
+const DateGroup = () => {
+  const { control } = useFormContext<AddDataForm>();
+  const dateObj = new Date();
   return (
-    <div className="w-fit!">
-      <div className="mb-3">
-        <div className="ml-1">
-          <DateDisplay date={dateObj} />
+    <Controller
+      control={control}
+      name="sDate"
+      render={({ field: { onChange } }) => (
+        <div className="w-fit!">
+          <div className="mb-3">
+            <div className="ml-1">
+              <DateDisplay date={dateObj} />
+            </div>
+            <div className="my-1.5 ml-0.5">
+              <DateLabel date={dateObj} />
+            </div>
+          </div>
+          <Separator className="mb-2" />
+          <CalendarPicker date={dateObj} setDate={onChange} />
         </div>
-        <div className="my-1.5 ml-0.5">
-          <DateLabel date={dateObj} />
-        </div>
-      </div>
-      <Separator className="mb-2" />
-      <CalendarPicker date={dateObj} setDate={setDate} />
-    </div>
+      )}
+    />
   );
 };
 
