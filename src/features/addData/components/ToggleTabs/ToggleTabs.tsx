ac300d45c7e42917a575c068651e76e78ecccadc
Cloud <monaniza@gmail.com>
2026-03-12 00:49:53 +0900

    fix ToggleTabsの選択中のデータ受け取れるように変更

--- src/features/addData/components/ToggleTabs/ToggleTabs.tsx
+++ src/features/addData/components/ToggleTabs/ToggleTabs.tsx
@@ -1,23 +1,35 @@
+import { AddDataForm } from "@/commons/addData/components/AddData";
 import { Tabs, TabsList, TabsTrigger } from "@/libs/shadcn/assets/ui/tabs";
+import { Controller, useFormContext } from "react-hook-form";
 
 const ToggleTabs = () => {
+  const { control } = useFormContext<AddDataForm>();
   return (
-    <Tabs defaultValue="start">
-      <TabsList className="h-8! rounded-sm">
-        <TabsTrigger
-          className="rounded-sm py-0 text-[11px] hover:text-muted-foreground"
-          value="start"
+    <Controller
+      control={control}
+      name="isStart"
+      render={({ field }) => (
+        <Tabs
+          value={field.value ? "start" : "end"}
+          onValueChange={(v) => field.onChange(v === "start")}
         >
-          開始基準
-        </TabsTrigger>
-        <TabsTrigger
-          className="rounded-sm py-0 text-[11px] hover:text-muted-foreground"
-          value="end"
-        >
-          終了基準
-        </TabsTrigger>
-      </TabsList>
-    </Tabs>
+          <TabsList className="h-8! rounded-sm">
+            <TabsTrigger
+              className="rounded-sm py-0 text-[11px] hover:text-muted-foreground"
+              value="start"
+            >
+              開始基準
+            </TabsTrigger>
+            <TabsTrigger
+              className="rounded-sm py-0 text-[11px] hover:text-muted-foreground"
+              value="end"
+            >
+              終了基準
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+      )}
+    />
   );
 };
 
