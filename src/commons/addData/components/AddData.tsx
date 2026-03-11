3f098f3633973cd772530ca106a5bf6175985dfb
Cloud <monaniza@gmail.com>
2026-03-12 00:52:12 +0900

    fix 子コンポーネントにデータを受けわせるようにProviderを追加、formの値を追加

--- src/commons/addData/components/AddData.tsx
+++ src/commons/addData/components/AddData.tsx
@@ -5,7 +5,7 @@ import { diffTime } from "@/commons/addData/entities/diffTime";
 import DateGroup from "@/features/addData/components/DateGroup/DateGroup";
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm, useWatch } from "react-hook-form";
+import { useForm, useWatch, FormProvider } from "react-hook-form";
 import {
   Field,
   FieldError,
@@ -47,22 +47,43 @@ const timeTextCn =
 /* 現状TimeZoneは日本のみを想定 */
 const TZ_OFFSET_JP = "09:00";
 
+/* バリデーション */
+const formSchema = z.object({
+  title: z.string(),
+  label: z.string(),
+  sDate: z.string(),
+  eDate: z.string(),
+  sTime: z.string(),
+  eTime: z.string(),
+  hTotal: z.number(),
+  mTotal: z.number(),
+  isStart: z.boolean(),
+});
+
+export type AddDataForm = z.infer<typeof formSchema>;
+
 const AddData = ({ labels }: Props) => {
   const [isEditing, setIsEditing] = useState(false);
+  const handleEditToggle = () => setIsEditing((prev) => !prev);
 
   const defaultDate = dayjs().tz("Asia/Tokyo");
   const defaultDateFormat = defaultDate.format("YYYY-MM-DD");
   const defaultTime = defaultDate.format("HH:mm");
   const defaultWorkTime = defaultDate.add(20, "minute").format("HH:mm");
 
-  /* バリデーション */
-  const formSchema = z.object({
-    title: z.string(),
-    label: z.string(),
-    sDate: z,
-    eDate: z,
-    sTime: z,
-    eTime: z,
+  const useFormMethods = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      label: labels[0],
+      sDate: defaultDateFormat,
+      eDate: defaultDateFormat,
+      sTime: defaultTime,
+      eTime: defaultWorkTime,
+      hTotal: 0,
+      mTotal: 0,
+      isStart: true,
+    },
   });
 
   const {
@@ -72,25 +93,7 @@ const AddData = ({ labels }: Props) => {
     reset,
     setValue,
     formState: { errors },
-  } = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      label: labels[0],
-      sDate: defaultDateFormat,
-      eDate: defaultDateFormat,
-      sTime: defaultTime,
-      eTime: defaultWorkTime,
-    },
-  });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: "",
-      description: "",
-    },
-  });
+  } = useFormMethods;
 
   const onSubmit = async (data: Form) => {
     try {
@@ -138,107 +141,109 @@ const AddData = ({ labels }: Props) => {
         </span>
       </div>
       <Line />
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex">
-          <FieldGroup className="py-6 px-7">
-            <Field>
-              <FieldLabel htmlFor="title">作業内容</FieldLabel>
-              <Input
-                id="title"
-                {...register("title", { required: true })}
-                placeholder="何をしていましたか？"
-                className="bg-background-2 text-sm py-5 px-3.25"
-              />
-              {errors.title && (
-                <FieldError>作業内容を入力してください</FieldError>
-              )}
-            </Field>
-            <Field>
-              <FieldLabel htmlFor="label">種類</FieldLabel>
-              <div className="gap-x-1.5 flex overflow-hidden mask-[linear-gradient(to_right,black_80%,transparent)]">
-                <input
-                  type="hidden"
-                  {...register("label", { required: true })}
+      <FormProvider {...useFormMethods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="flex">
+            <FieldGroup className="py-6 px-7">
+              <Field>
+                <FieldLabel htmlFor="title">作業内容</FieldLabel>
+                <Input
+                  id="title"
+                  {...register("title", { required: true })}
+                  placeholder="何をしていましたか？"
+                  className="bg-background-2 text-sm py-5 px-3.25"
                 />
-                {labels.map((label) => (
-                  <TaskBtn
-                    key={label}
-                    label={label}
-                    setLabel={label === labelValue}
-                    onClick={() => setValue("label", label)}
-                  />
-                ))}
-                {errors.label && (
-                  <FieldError>種類を選択してください</FieldError>
+                {errors.title && (
+                  <FieldError>作業内容を入力してください</FieldError>
                 )}
-              </div>
-            </Field>
-            <Field className="my-auto">
-              <FieldLabel htmlFor="time">作業時間</FieldLabel>
-              <div className="bg-background-2 rounded-2xl  border-[1.5px] overflow-hidden">
-                <div className="flex justify-between ">
-                  <TimeField text={"開始"} />
+              </Field>
+              <Field>
+                <FieldLabel htmlFor="label">種類</FieldLabel>
+                <div className="gap-x-1.5 flex overflow-hidden mask-[linear-gradient(to_right,black_80%,transparent)]">
                   <input
-                    type="time"
-                    id="sTime"
-                    {...register("sTime", { required: true })}
-                    className={timeTextCn}
+                    type="hidden"
+                    {...register("label", { required: true })}
                   />
+                  {labels.map((label) => (
+                    <TaskBtn
+                      key={label}
+                      label={label}
+                      setLabel={label === labelValue}
+                      onClick={() => setValue("label", label)}
+                    />
+                  ))}
+                  {errors.label && (
+                    <FieldError>種類を選択してください</FieldError>
+                  )}
                 </div>
-                <Line />
-                <div className="flex justify-between">
-                  <TimeField text={"終了"} />
-                  <input
-                    type="time"
-                    id="eTime"
-                    {...register("eTime", { required: true })}
-                    className={timeTextCn}
-                  />
+              </Field>
+              <Field className="my-auto">
+                <FieldLabel htmlFor="time">作業時間</FieldLabel>
+                <div className="bg-background-2 rounded-2xl  border-[1.5px] overflow-hidden">
+                  <div className="flex justify-between ">
+                    <TimeField text={"開始"} />
+                    <input
+                      type="time"
+                      id="sTime"
+                      {...register("sTime", { required: true })}
+                      className={timeTextCn}
+                    />
+                  </div>
+                  <Line />
+                  <div className="flex justify-between">
+                    <TimeField text={"終了"} />
+                    <input
+                      type="time"
+                      id="eTime"
+                      {...register("eTime", { required: true })}
+                      className={timeTextCn}
+                    />
+                  </div>
+                  <Line dashed />
+                  <div className="flex justify-between items-center px-6 py-2.25">
+                    <div className="text-2xs font-medium text-subtle-foreground">
+                      合計
+                    </div>
+                    <div className="flex gap-5 items-center">
+                      <span className="font-dm-mono text-sm text-muted-foreground font-medium">
+                        {workingTime}
+                      </span>
+                      <EditBtn onClick={handleEditToggle} />
+                    </div>
+                  </div>
+                  {isEditing && (
+                    <div>
+                      <Line dashed />
+                      <EditContainer />
+                    </div>
+                  )}
                 </div>
-                <Line dashed />
-                <div className="flex justify-between items-center px-6 py-2.25">
-                  <div className="text-2xs font-medium text-subtle-foreground">
-                    合計
-                  </div>
-                  <div className="flex gap-5 items-center">
-                    <span className="font-dm-mono text-sm text-muted-foreground font-medium">
-                      {workingTime}
-                    </span>
-                    <EditBtn onClick={() => setIsEditing((prev) => !prev)} />
-                  </div>
-                </div>
-                {isEditing && (
-                  <div>
-                    <Line dashed />
-                    <EditContainer hours={2} minutes={0} />
-                  </div>
-                )}
-              </div>
-            </Field>
-          </FieldGroup>
-          <Line vertical />
-          <div className="bg-background-2 w-[320px] min-h-113.25">
-            <Field className="pt-6 px-5">
-              <FieldLabel htmlFor="date">日付</FieldLabel>
-              <DateGroup date={sDateValue} setDate={handleSetDate} />
-            </Field>
+              </Field>
+            </FieldGroup>
+            <Line vertical />
+            <div className="bg-background-2 w-[320px] min-h-113.25">
+              <Field className="pt-6 px-5">
+                <FieldLabel htmlFor="date">日付</FieldLabel>
+                <DateGroup date={sDateValue} setDate={handleSetDate} />
+              </Field>
+            </div>
           </div>
-        </div>
-        <Line />
-        <div className="flex w-full px-7 py-4.5 gap-2.5 justify-between">
-          <input
-            type="submit"
-            value="記録を追加"
-            className="rounded-lg py-2.5 w-full px-5 bg-primary text-background text-[14px] font-medium cursor-pointer"
-          />
-          <input
-            type="button"
-            onClick={() => reset()}
-            value="キャンセル"
-            className="rounded-lg py-2.5 px-5 border border-border text-foreground text-[14px] font-medium cursor-pointer max-w-30"
-          />
-        </div>
-      </form>
+          <Line />
+          <div className="flex w-full px-7 py-4.5 gap-2.5 justify-between">
+            <input
+              type="submit"
+              value="記録を追加"
+              className="rounded-lg py-2.5 w-full px-5 bg-primary text-background text-[14px] font-medium cursor-pointer"
+            />
+            <input
+              type="button"
+              onClick={() => reset()}
+              value="キャンセル"
+              className="rounded-lg py-2.5 px-5 border border-border text-foreground text-[14px] font-medium cursor-pointer max-w-30"
+            />
+          </div>
+        </form>
+      </FormProvider>
     </div>
   );
 };
