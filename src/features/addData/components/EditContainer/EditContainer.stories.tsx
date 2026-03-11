3f52463b618212f5a69a83500dd54f298cf8826a
Cloud <monaniza@gmail.com>
2026-03-12 01:39:52 +0900

    fix editContener修正

--- src/features/addData/components/EditContainer/EditContainer.stories.tsx
+++ src/features/addData/components/EditContainer/EditContainer.stories.tsx
@@ -1,20 +1,36 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-
+import { FormProvider, useForm } from "react-hook-form";
 import EditContainer from "./EditContainer";
+import type { AddDataForm } from "@/commons/addData/components/AddData";
 
 const meta = {
   component: EditContainer,
   parameters: {
     layout: "fullscreen",
   },
+  decorators: [
+    (Story) => {
+      const methods = useForm<AddDataForm>({
+        defaultValues: {
+          title: "",
+          label: "",
+          sDate: "",
+          eDate: "",
+          sTime: "",
+          eTime: "",
+          total: 0,
+        },
+      });
+      return (
+        <FormProvider {...methods}>
+          <Story />
+        </FormProvider>
+      );
+    },
+  ],
 } satisfies Meta<typeof EditContainer>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {
-    hours: 2,
-    minutes: 0,
-  },
-};
+export const Default: Story = {};
