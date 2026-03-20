49d2975f9b695120903cbe682e31430700bdc89a
Cloud <monaniza@gmail.com>
2026-03-20 15:15:32 +0900

    fix 不要なimport削除

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { DM_Mono, DM_Sans, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Sidebar from "@/commons/Sidebar/components/Sidebar";
-import { contents } from "@/commons/Sidebar/constants/contents";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
