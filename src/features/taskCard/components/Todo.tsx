6e342da993679cf83879971766236d5afcfa259e
Cloud <monaniza@gmail.com>
2026-03-20 18:42:53 +0900

    feat TaskCardで出し分けするTODOの部分

---
+++ src/features/taskCard/components/Todo.tsx
@@ -0,0 +1,28 @@
+import PriorityBadge from "@/commons/priorityBadge/components/PriorityBadge";
+import { Checkbox } from "@/libs/shadcn/assets/ui/checkbox";
+
+type Props = {
+  time: string;
+  work: string;
+  priority?: "high" | "medium" | "low";
+};
+
+const Todo = ({ time, work, priority }: Props) => {
+  return (
+    <>
+      <Checkbox className="size-5.5 rounded-[6px] border-[#909097]" />
+      <div>
+        <PriorityBadge priority={priority} />
+      </div>
+      <div className="flex flex-col gap-0.5 w-full">
+        <span className="text-[#FAFAF9] text-sm">{time}</span>
+        <span className="text-[#8E8E93] text-xs">{work}</span>
+      </div>
+      <div>
+        <div className="w-8 h-8 bg-[#4A4A50] rounded-3xl">→</div>
+      </div>
+    </>
+  );
+};
+
+export default Todo;
