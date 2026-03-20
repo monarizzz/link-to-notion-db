e6c3ca6ca4a7c32e40dc20055ee157344e971ac6
Cloud <monaniza@gmail.com>
2026-03-20 19:03:02 +0900

    fix typo

--- src/commons/currentDate/components/CurrentDate.stories.tsx
+++ src/commons/currentDate/components/CurrentDate.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
-import CurrentDate from "./currentDate";
+import CurrentDate from "./CurrentDate";
 
 const meta = {
   component: CurrentDate,
