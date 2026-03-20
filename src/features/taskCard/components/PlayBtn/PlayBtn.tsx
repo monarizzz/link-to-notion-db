dcbc4893b0e3b033b789b10e9a1d83314366614a
Cloud <monaniza@gmail.com>
2026-03-20 22:42:24 +0900

    feat notionが想定する形に変換する

--- src/features/taskCard/components/PlayBtn/PlayBtn.tsx
+++ src/features/taskCard/components/PlayBtn/PlayBtn.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
+import dayjs from "dayjs";
+import "@/libs/dayjs/config";
 
 const PlayBtn = () => {
   return (
     <button
       className="bg-[#c2ef53ff] shadow-[0_0_12px_rgba(194,239,83,0.2)] p-3 rounded-3xl"
-      onClick={() => console.log(Date.now())}
+      onClick={() =>
+        console.log(dayjs().tz("Asia/Tokyo").format("YYYY-MM-DDTHH:mm:ss.SSSZ"))
+      }
     >
       <Image src="/play.svg" width={16} height={16} alt="play" />
       {/* {time} */}
