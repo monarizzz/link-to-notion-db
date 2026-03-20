0f6a656b93b434356dee16a613636ec5c298801e
Cloud <monaniza@gmail.com>
2026-03-20 19:26:19 +0900

    fix importの追加

--- src/commons/currentDate/entities/getCurrentDate.ts
+++ src/commons/currentDate/entities/getCurrentDate.ts
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import "@/libs/dayjs/config";
 
 export const getCurrentDate = () => {
   return dayjs().tz("Asia/Tokyo").format("YYYY.MM.DD");
--- src/commons/currentTime/entities/getCurrentTime.ts
+++ src/commons/currentTime/entities/getCurrentTime.ts
@@ -1,4 +1,5 @@
 import dayjs from "dayjs";
+import "@/libs/dayjs/config";
 
 export const getCurrentTime = () => {
   return dayjs().tz("Asia/Tokyo").format("HH:mm");
