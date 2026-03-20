f247e2cb352bb283ea8a647fe01410df51ba45f7
Cloud <monaniza@gmail.com>
2026-03-21 00:12:31 +0900

    fix padding変更

--- src/features/taskCard/components/doingContent/doingContent.tsx
+++ src/features/taskCard/components/doingContent/doingContent.tsx
@@ -24,7 +24,7 @@ const DoingContent = ({ work, time }: Props) => {
           <span className="text-[#6B6B70] text-[11px]">{time}</span>
         </div>
       </div>
-      <div className="bg-[#357ff6ff] shadow-[0_0_12px_rgba(53,127,246,0.2)] p-0.5 rounded-3xl">
+      <div className="bg-[#357ff6ff] shadow-[0_0_12px_rgba(53,127,246,0.2)] p-[3.5px] rounded-3xl">
         <Image src="/check.svg" width={39} height={39} alt="check" />
       </div>
     </>
