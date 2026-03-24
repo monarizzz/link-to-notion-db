18e533a1e228a4571344b206e2ac19d76b9a952f
Cloud <monaniza@gmail.com>
2026-03-24 19:02:06 +0900

    fix storybook

--- src/features/taskCard/components/StopBtn/StopBtn.stories.tsx
+++ src/features/taskCard/components/StopBtn/StopBtn.stories.tsx
@@ -1,13 +1,25 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
+import { FormProvider, useForm } from "react-hook-form";
 
 import StopBtn from "./StopBtn";
 
+const WithFormProvider = () => {
+  const methods = useForm();
+  return (
+    <FormProvider {...methods}>
+      <form>
+        <StopBtn />
+      </form>
+    </FormProvider>
+  );
+};
+
 const meta = {
-  component: StopBtn,
+  component: WithFormProvider,
   parameters: {
     layout: "fullscreen",
   },
-} satisfies Meta<typeof StopBtn>;
+} satisfies Meta<typeof WithFormProvider>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
