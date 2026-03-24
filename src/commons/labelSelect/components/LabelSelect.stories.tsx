0faa3a853125f6ad211d776c7653082ecd6cf330
Cloud <monaniza@gmail.com>
2026-03-24 23:23:21 +0900

    fix add provieder

--- src/commons/labelSelect/components/LabelSelect.stories.tsx
+++ src/commons/labelSelect/components/LabelSelect.stories.tsx
@@ -1,11 +1,22 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { FormProvider, useForm } from "react-hook-form";
 
 import LabelSelect from "./LabelSelect";
 
+const FormDecorator = (Story: React.ComponentType) => {
+  const methods = useForm();
+  return (
+    <FormProvider {...methods}>
+      <Story />
+    </FormProvider>
+  );
+};
+
 const meta: Meta<typeof LabelSelect> = {
   component: LabelSelect,
+  decorators: [FormDecorator],
   parameters: {
-    layout: "centered",
+    layout: "fullscreen",
   },
 };
 
