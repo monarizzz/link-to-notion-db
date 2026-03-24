7e0dc06e1776e42acffbc28d38d30d2c19381b84
Cloud <monaniza@gmail.com>
2026-03-25 00:49:34 +0900

    ref directory

--- src/features/workCard/components/inputContent/InputContent.tsx
+++ src/features/workCard/components/inputContent/InputContent.tsx
@@ -1,4 +1,4 @@
-import LabelSelect from "@/features/modal/LabelSelect/LabelSelect";
+import LabelSelect from "@/features/modal/components/LabelSelect/LabelSelect";
 import PlayBtn from "../PlayBtn/PlayBtn";
 import Pen from "@public/pen.svg";
 
