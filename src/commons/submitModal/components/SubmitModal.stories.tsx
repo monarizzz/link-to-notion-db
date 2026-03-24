d6f4b32e492728df2849b0687736a1b88a732ae1
Cloud <monaniza@gmail.com>
2026-03-24 18:52:15 +0900

    feat 命名とディレクトリ整理, stateで開閉

--- src/commons/submitModal/components/SubmitModal.stories.tsx
+++ src/commons/submitModal/components/SubmitModal.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
 import { useForm, FormProvider } from "react-hook-form";
 
-import CheckoutModal from "./CheckoutModal";
+import CheckoutModal from "./SubmitModal";
 
 const meta: Meta<typeof CheckoutModal> = {
   component: CheckoutModal,
