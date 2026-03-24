7923cb487bd8978152c8ae46e09de188ce351d91
Cloud <monaniza@gmail.com>
2026-03-24 23:28:05 +0900

    ref ディレクトリ修正

--- src/features/home/components/HomePageMain/HomePageMain.tsx
+++ src/features/home/components/HomePageMain/HomePageMain.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { addDataAction } from "@/commons/addData/entities/addDataAction";
-
+import { addDataAction } from "@/commons/submitModal/entities/addDataAction";
 import BottomBar from "@/commons/layout/components/BottomBar/BottomBar";
 import NowDisplay from "../NowDisplay/NowDisplay";
 import TaskCard from "@/commons/taskCard/components/TaskCard";
