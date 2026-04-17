8abc4d31f204a09dafb3f59d70efea52cda75824
Cloud <monaniza@gmail.com>
2026-04-17 12:24:54 +0900

    fix typo

--- src/features/home/components/HomePageMain/HomePageMain.tsx
+++ src/features/home/components/HomePageMain/HomePageMain.tsx
@@ -88,11 +88,7 @@ const HomePageMain = ({ labels, events }: Props) => {
 =======
           <div className="flex flex-row bg-[#1A1A1A] h-screen">
             <div className="shrink-0 my-auto">
-<<<<<<< HEAD
-              {/* <SideBar event={} />/ */}
-=======
-              <SideBar />
->>>>>>> 5542e26 (feat sidebar作成)
+              <SideBar today={null} />
             </div>
             <div className="mx-16 mt-10">
 >>>>>>> 5542e26 (feat sidebar作成)
