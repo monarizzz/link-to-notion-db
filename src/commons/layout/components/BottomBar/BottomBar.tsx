98345dff66ed073f76fa853cd3a5f74b998ba6ee
Cloud <monaniza@gmail.com>
2026-04-17 15:36:07 +0900

    feat 遷移先を設定

--- src/commons/layout/components/BottomBar/BottomBar.tsx
+++ src/commons/layout/components/BottomBar/BottomBar.tsx
@@ -6,13 +6,13 @@ const text = "text-[13px] text-foreground italic font-semibold font-dm-sans";
 const BottomBar = () => {
   return (
     <div className="gap-8 flex bg-[#ffffff0a] rounded-[40px] py-3 px-7 w-fit border-[#FFFFFF25] border">
-      <Link href="./">
+      <Link href="/">
         <span className={text}>Todo</span>
       </Link>
-      <Link href="./">
+      <Link href="/Calendar">
         <span className={text}>Calendar</span>
       </Link>
-      <Link href="./">
+      <Link href="/Board">
         <span className={text}>Board</span>
       </Link>
     </div>
